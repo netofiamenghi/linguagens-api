e436488c10f0832599e2979df43092529aa2cbfa
antonioneto <neto.fiamenghi@gmail.com>
2023-10-01 23:18:57 -0300

    Inserindo imagens por lista

--- src/main/java/com/ghitecnologia/linguagensapi/LinguagemControle.java
+++ src/main/java/com/ghitecnologia/linguagensapi/LinguagemControle.java
@@ -3,12 +3,18 @@ package com.ghitecnologia.linguagensapi;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class LinguagemControle {
 
-    @GetMapping(value = "/linguagem-preferida")
-    public String processaLinguagemPreferida(){
-        return "Oi, Java!";
-    }
+    private List<Linguagem> linguagens = List.of(
+            new Linguagem("Java", "https://raw.githubusercontent.com/abrahamcalf/programming-languages-logos/master/src/java/java_256x256.png", 1),
+            new Linguagem("PHP", "https://raw.githubusercontent.com/abrahamcalf/programming-languages-logos/master/src/php/php_256x256.png", 2)
+    );
 
+    @GetMapping("/linguagens")
+    public List<Linguagem> obterLinguagens(){
+        return linguagens;
+    }
 }
