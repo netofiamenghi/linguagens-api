1b30c5e2a01faa5d6bc6b81e25b7e00a6efc5621
antonioneto <neto.fiamenghi@gmail.com>
2023-10-02 22:55:51 -0300

    Add repository

--- src/main/java/com/ghitecnologia/linguagensapi/Linguagem.java
+++ src/main/java/com/ghitecnologia/linguagensapi/Linguagem.java
@@ -2,9 +2,16 @@ package com.ghitecnologia.linguagensapi;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
+@Document(collection = "principaisLinguagens")
 @AllArgsConstructor
 public class Linguagem {
+
+    @Id
+    @Getter
+    private String id;
 
     @Getter
     private String title;
--- src/main/java/com/ghitecnologia/linguagensapi/LinguagemControle.java
+++ src/main/java/com/ghitecnologia/linguagensapi/LinguagemControle.java
@@ -1,5 +1,6 @@
 package com.ghitecnologia.linguagensapi;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,13 +9,12 @@ import java.util.List;
 @RestController
 public class LinguagemControle {
 
-    private List<Linguagem> linguagens = List.of(
-            new Linguagem("Java", "https://raw.githubusercontent.com/abrahamcalf/programming-languages-logos/master/src/java/java_256x256.png", 1),
-            new Linguagem("PHP", "https://raw.githubusercontent.com/abrahamcalf/programming-languages-logos/master/src/php/php_256x256.png", 2)
-    );
+    @Autowired
+    private LinguagemRepository repository;
 
     @GetMapping("/linguagens")
     public List<Linguagem> obterLinguagens(){
+        List<Linguagem> linguagens = repository.findAll();
         return linguagens;
     }
 }
